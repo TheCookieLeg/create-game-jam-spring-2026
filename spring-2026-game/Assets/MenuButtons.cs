2350b8bcf322936d35ef434e15c4ae2c9a87dca9
paragoncode <54541723+paragoncode@users.noreply.github.com>
2026-03-21 20:23:58 +0100

    pause menu

--- spring-2026-game/Assets/MenuButtons.cs
+++ spring-2026-game/Assets/MenuButtons.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
